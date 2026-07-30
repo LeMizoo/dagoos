7af2bd1532c8bb29c4b80e9ccd4afd95b3c4acc0
LeMizoo <Tovoniaina.rahendrison@gmail.com>
2026-07-30 08:37:46 +0300

    fix: création dossier api/public/stats

--- admin-next/.next/server/server-reference-manifest.js
+++ admin-next/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"CnBE0b7Qo6Q6jNK3oZYCun9YQS0aSjKGgNMOq6o4Kug=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"I5L/6Gk8vbE2mkWvBETvWQICnOzs42hWPq2RRaQI1M0=\"}"
