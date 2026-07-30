b076991ad61c958bc7b6763668ecf6ad36616f21
LeMizoo <Tovoniaina.rahendrison@gmail.com>
2026-07-30 15:24:54 +0300

    chore: nettoyage dossiers legacy (fleet, coop, landing, admin, public, shared)

--- apps/coop/pages/deliveries.js
+++
@@ -1,3 +0,0 @@
-function init_deliveries() {
-    document.getElementById('mainContent').innerHTML = '<div class="topbar"><h1>📦 Livraisons</h1></div><div class="card" style="text-align:center;padding:60px;">Bientôt disponible</div>';
-}
