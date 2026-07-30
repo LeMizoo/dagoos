32353511ba73655c77603f3bf0c3d818138bf1c2
LeMizoo <Tovoniaina.rahendrison@gmail.com>
2026-07-30 11:36:24 +0300

    fix: suppression prefetch nextjs dans TrustSection

--- admin-next/.next/server/server-reference-manifest.js
+++ admin-next/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"ZDEE6hkudYDE3GUkMoyP4onK0TLjEpssmwhRWey3r1c=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"G4+4Mydn1XRZ3VHiygkCXKdpa3b6Nn3+aSwmIJtHYzw=\"}"
