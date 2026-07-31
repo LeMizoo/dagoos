1c633d0fd228922c082bc661347f443c0a0b9bf4
LeMizoo <Tovoniaina.rahendrison@gmail.com>
2026-07-31 11:21:40 +0300

    fix: codes uniques seed-coops + import authMiddleware

--- apps/api/modules/auth/auth.routes.js
+++ apps/api/modules/auth/auth.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const prisma = require('../../lib/prisma');
-const { JWT_SECRET } = require('../../middleware/auth');
+const { authMiddleware, JWT_SECRET } = require('../../middleware/auth');
 
 const router = express.Router();
 
