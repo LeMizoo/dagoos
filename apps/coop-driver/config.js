fe7c7db2976ed9f0bbbabbdca762c44017af9590
LeMizoo <Tovoniaina.rahendrison@gmail.com>
2026-08-21 09:23:58 +0300

    Fix: config.js coop-driver remis en production

--- apps/coop-driver/config.js
+++ apps/coop-driver/config.js
@@ -1,5 +1,5 @@
 window.DAGOOS_CONFIG = {
-    apiUrl: 'http://localhost:3000/api',
+    apiUrl: 'https://dagoos-api.onrender.com/api',
     landingUrl: 'https://dago-mobility.vercel.app',
     logoUrl: 'https://dago-mobility.vercel.app/icons/b-trans.svg'
 };
